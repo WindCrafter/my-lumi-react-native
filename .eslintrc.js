6e2993f923223efd35b9ce8b07840fe07d3d5019
= <=>
2020-12-16 12:19:17 +0700

    them esslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
   root: true,
-  "extends": "@nghinv/eslint-config", 
-
+  extends: ['@nghinv/eslint-config', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
 };
