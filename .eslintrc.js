bc6e7753f3fc75473e20666c2c1a3e1f8d7af009
vinhduongnuce <vinh268461@nuce.edu.vn>
2020-12-17 08:43:14 +0700

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,4 @@
 module.exports = {
   root: true,
-  extends: ['@nghinv/eslint-config', 'prettier'],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': ['error'],
-  },
+  extends: '@react-native-community',
 };
