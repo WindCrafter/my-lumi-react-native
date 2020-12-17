d1576b3c149b5a1a12e48a17249147588e248fd1
vinhduongnuce <vinh268461@nuce.edu.vn>
2020-12-17 16:39:36 +0700

    update

---
+++ utlis/config/bank.js
@@ -0,0 +1,386 @@
+export const BANK_LIST = [
+  {
+    MaNganHang: '207',
+    TenNH: 'NH Chinh sach xa hoi (VBSP)',
+  },
+  {
+    MaNganHang: '201',
+    TenNH: 'NH Cong thuong VN (Vietinbank)',
+  },
+  {
+    MaNganHang: '202',
+    TenNH: 'NH Dau tu va Phat trien VN (BIDV)',
+  },
+  {
+    MaNganHang: '204',
+    TenNH: 'NH NNo&PT Nong thon VN-AGribank',
+  },
+  {
+    MaNganHang: '208',
+    TenNH: 'NH Phat trien Viet Nam (VDB)',
+  },
+  {
+    MaNganHang: '',
+    TenNH: 'Ngan hang TMCP Ngoai thuong Viet Nam',
+  },
+  {
+    MaNganHang: '359',
+    TenNH: 'NH BAO VIET (Bao Viet Bank)',
+  },
+  {
+    MaNganHang: '358',
+    TenNH: 'NH Tien Phong (Tien Phong Bank)',
+  },
+  {
+    MaNganHang: '339',
+    TenNH: 'NH TM TNHH MTV Xay Dung VN(CB Bank)',
+  },
+  {
+    MaNganHang: '327',
+    TenNH: 'NH TMCP Ban Viet',
+  },
+  {
+    MaNganHang: '357',
+    TenNH: 'NH TMCP BUU DIEN LIEN VIET',
+  },
+  {
+    MaNganHang: '360',
+    TenNH: 'NH TMCP Dai Chung (PVcombank)',
+  },
+  {
+    MaNganHang: '352',
+    TenNH: 'NH TMCP Quoc Dan (Nam Viet cu)',
+  },
+  {
+    MaNganHang: '355',
+    TenNH: 'NH TMCP Viet A',
+  },
+  {
+    MaNganHang: '356',
+    TenNH: 'NH TMCP Viet Nam Thuong Tin (Vietbank)',
+  },
+  {
+    MaNganHang: '307',
+    TenNH: 'NHTMCP A Chau (ACB)',
+  },
+  {
+    MaNganHang: '323',
+    TenNH: 'NHTMCP An Binh (ABBank)',
+  },
+  {
+    MaNganHang: '313',
+    TenNH: 'NHTMCP Bac A (Bac A bank)',
+  },
+  {
+    MaNganHang: '319',
+    TenNH: 'NHTMCP Dai Duong (Oceanbank)',
+  },
+  {
+    MaNganHang: '320',
+    TenNH: 'NHTMCP Dau khi Toan cau (GPBank)',
+  },
+  {
+    MaNganHang: '304',
+    TenNH: 'NHTMCP Dong A (Dong A bank)',
+  },
+  {
+    MaNganHang: '317',
+    TenNH: 'NHTMCP Dong Nam A (Seabank)',
+  },
+  {
+    MaNganHang: '302',
+    TenNH: 'NHTMCP Hang Hai (Maritime Bank)',
+  },
+  {
+    MaNganHang: '353',
+    TenNH: 'NHTMCP Kien Long (Kien Long bank)',
+  },
+  {
+    MaNganHang: '310',
+    TenNH: 'NHTMCP Ky thuong VN (Techcombank)',
+  },
+  {
+    MaNganHang: '306',
+    TenNH: 'NHTMCP Nam A (Nam A Bank)',
+  },
+  {
+    MaNganHang: '321',
+    TenNH: 'NHTMCP phat trien Tp HCM (HD Bank)',
+  },
+  {
+    MaNganHang: '333',
+    TenNH: 'NHTMCP Phuong Dong (OCB)',
+  },
+  {
+    MaNganHang: '311',
+    TenNH: 'NHTMCP Quan Doi (MB)',
+  },
+  {
+    MaNganHang: '314',
+    TenNH: 'NHTMCP Quoc Te (VIB)',
+  },
+  {
+    MaNganHang: '334',
+    TenNH: 'NHTMCP Sai Gon (SCB)',
+  },
+  {
+    MaNganHang: '348',
+    TenNH: 'NHTMCP Sai gon - Ha Noi (SHB)',
+  },
+  {
+    MaNganHang: '303',
+    TenNH: 'NHTMCP Sai gon Thuong Tin (Sacombank)',
+  },
+  {
+    MaNganHang: '308',
+    TenNH: 'NHTMCP SG Cong Thuong (SaigonBank)',
+  },
+  {
+    MaNganHang: '309',
+    TenNH: 'NHTMCP VN Thinh Vuong(VP Bank)',
+  },
+  {
+    MaNganHang: '341',
+    TenNH: 'NHTMCP Xang dau Petrolimex (PGBank)',
+  },
+  {
+    MaNganHang: '305',
+    TenNH: 'NHTMCP Xuat Nhap Khau (Eximbank)',
+  },
+  {
+    MaNganHang: '502',
+    TenNH: 'INDOVINA BANK',
+  },
+  {
+    MaNganHang: '505',
+    TenNH: 'NH lien doanh Viet Nga',
+  },
+  {
+    MaNganHang: '504',
+    TenNH: 'NH lien doanh Viet Thai',
+  },
+  {
+    MaNganHang: '501',
+    TenNH: 'PUBLIC BANK VN',
+  },
+  {
+    MaNganHang: '664',
+    TenNH: 'Agricultural Bank of China Ltd (ABOC)',
+  },
+  {
+    MaNganHang: '602',
+    TenNH: 'ANZ BANK',
+  },
+  {
+    MaNganHang: '612',
+    TenNH: 'BANGKOK BANK',
+  },
+  {
+    MaNganHang: '620',
+    TenNH: 'BANK OF CHINA',
+  },
+  {
+    MaNganHang: '615',
+    TenNH: 'Bank of Communications',
+  },
+  {
+    MaNganHang: '659',
+    TenNH: 'Bank of India',
+  },
+  {
+    MaNganHang: '657',
+    TenNH: 'BNP Paribas Chi nhanh HN',
+  },
+  {
+    MaNganHang: '614',
+    TenNH: 'BNP- PARIBAS CN HCM',
+  },
+  {
+    MaNganHang: '601',
+    TenNH: 'BPCE IOM',
+  },
+  {
+    MaNganHang: '611',
+    TenNH: 'China Construction Bank Corporation',
+  },
+  {
+    MaNganHang: '661',
+    TenNH: 'CIMB BANK',
+  },
+  {
+    MaNganHang: '654',
+    TenNH: 'CITI BANK',
+  },
+  {
+    MaNganHang: '605',
+    TenNH: 'CITI BANK HN',
+  },
+  {
+    MaNganHang: '621',
+    TenNH: 'Credit Agricole CIB',
+  },
+  {
+    MaNganHang: '619',
+    TenNH: 'DEUTSCHE BANK',
+  },
+  {
+    MaNganHang: '630',
+    TenNH: 'FIRST COMMERCIAL BANK HCM',
+  },
+  {
+    MaNganHang: '617',
+    TenNH: 'HONGKONG AND SHANGHAI BANK (HSBC)',
+  },
+  {
+    MaNganHang: '640',
+    TenNH: 'HUANAN COMMERECIAL BANK LTD chi nhanh SG',
+  },
+  {
+    MaNganHang: '641',
+    TenNH: 'Industrial Bank of korea',
+  },
+  {
+    MaNganHang: '627',
+    TenNH: 'JP MORGAN CHASE BANK',
+  },
+  {
+    MaNganHang: '626',
+    TenNH: 'KEB HANA BANK',
+  },
+  {
+    MaNganHang: '635',
+    TenNH: 'MALAYAN BANKING BERHAD HCM',
+  },
+  {
+    MaNganHang: '609',
+    TenNH: 'MAY BANK (HN)',
+  },
+  {
+    MaNganHang: '623',
+    TenNH: 'MEGA INTERNATIONAL COMMERCIAL Co.LTD',
+  },
+  {
+    MaNganHang: '613',
+    TenNH: 'Mizuho Corporate Bank Ltd., HN',
+  },
+  {
+    MaNganHang: '639',
+    TenNH: 'MIZUHO CORPORATE BANK, LTD',
+  },
+  {
+    MaNganHang: '660',
+    TenNH: 'Ngan hang Busan',
+  },
+  {
+    MaNganHang: '653',
+    TenNH: 'Ngan hang MUFG Bank, LTD CN HN',
+  },
+  {
+    MaNganHang: '656',
+    TenNH: 'NH  Hana',
+  },
+  {
+    MaNganHang: '634',
+    TenNH: 'NH CATHAY UNITED',
+  },
+  {
+    MaNganHang: '652',
+    TenNH: 'NH Cong nghiep Han Quoc',
+  },
+  {
+    MaNganHang: '650',
+    TenNH: 'NH DBS Bank Ltd CN HCM',
+  },
+  {
+    MaNganHang: '638',
+    TenNH: 'NH DTPT Campuchia  Ha Noi',
+  },
+  {
+    MaNganHang: '648',
+    TenNH: 'NH DTPT Campuchia - HCM',
+  },
+  {
+    MaNganHang: '632',
+    TenNH: 'NH FAR EAST NATIONAL BANK ',
+  },
+  {
+    MaNganHang: '608',
+    TenNH: 'NH FIRST COMMERCIAL BANK HANOI',
+  },
+  {
+    MaNganHang: '649',
+    TenNH: 'NH Industrial & Commercial Bank of China',
+  },
+  {
+    MaNganHang: '631',
+    TenNH: 'NH Kookmin  TP Ho Chi Minh',
+  },
+  {
+    MaNganHang: '622',
+    TenNH: 'NH MUFG BANK ,LTD CN HCM',
+  },
+  {
+    MaNganHang: '662',
+    TenNH: 'NH NongHyup',
+  },
+  {
+    MaNganHang: '642',
+    TenNH: 'NH Taipei FubonC.B',
+  },
+  {
+    MaNganHang: '655',
+    TenNH: 'NH TM Taipei Fubon',
+  },
+  {
+    MaNganHang: '658',
+    TenNH: 'NH TM TNHH E.Sun',
+  },
+  {
+    MaNganHang: '629',
+    TenNH: 'NH TNHH CTBC ( NHTM Chinatrust)',
+  },
+  {
+    MaNganHang: '616',
+    TenNH: 'NH TNHH MTV Shinhan VN',
+  },
+  {
+    MaNganHang: '665',
+    TenNH: 'NH TNHH UOB VIETNAM',
+  },
+  {
+    MaNganHang: '651',
+    TenNH: 'NHTM Taipei Fubon',
+  },
+  {
+    MaNganHang: '603',
+    TenNH: 'NHTNHH MTV Hong Leong VN',
+  },
+  {
+    MaNganHang: '625',
+    TenNH: 'OVERSEA - CHINESE BANKING CORP Ltd',
+  },
+  {
+    MaNganHang: '600',
+    TenNH: 'Siam Commercial Bank Public Company Ltd',
+  },
+  {
+    MaNganHang: '604',
+    TenNH: 'STANDARD CHARTERED BANK',
+  },
+  {
+    MaNganHang: '636',
+    TenNH: 'SUMITOMO MITSUI BANKING CORPORATION',
+  },
+  {
+    MaNganHang: '606',
+    TenNH: 'The Shanghai Com&Savings Bank',
+  },
+  {
+    MaNganHang: '618',
+    TenNH: 'UOB CN HCM',
+  },
+  {
+    MaNganHang: '663',
+    TenNH: 'Woori Bank Viet Nam',
+  },
+];
